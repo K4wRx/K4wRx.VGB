9d0c548aae89c1f3c3c66d963b27fbd36c02f47e
fumito_ito <weathercook@gmail.com>
2015-04-26 01:12:54 +0900

    remove origin methods

--- K4wRx.VGB/Extensions/KinectSensorExtension.cs
+++ K4wRx.VGB/Extensions/KinectSensorExtension.cs
@@ -14,119 +14,6 @@ namespace K4wRx.Extensions
     {
         private static CompositeDisposable disposables = new CompositeDisposable();
         /// <summary>
-        /// Create KinectSensor property changed stream from KinectSensor
-        /// </summary>
-        /// <param name="sensor">source of stream</param>
-        /// <returns>Observable KinectSensor property changes stream</returns>
-        public static IObservable<PropertyChangedEventArgs> AsObservable(this KinectSensor sensor)
-        {
-            return Observable.FromEventPattern<PropertyChangedEventHandler, PropertyChangedEventArgs>(
-                h => sensor.PropertyChanged += h,
-                h => sensor.PropertyChanged -= h
-                ).Select(e => e.EventArgs);
-        }
-        /// <summary>
-        /// Create KinectSensor availability stream from KinectSensor.
-        /// </summary>
-        /// <param name="sensor">source of stream</param>
-        /// <returns>Observable Kinect availability stream</returns>
-        public static IObservable<IsAvailableChangedEventArgs> AvailabilityAsObservable(this KinectSensor sensor)
-        {
-            return Observable.FromEventPattern<IsAvailableChangedEventArgs>(
-                h => sensor.IsAvailableChanged += h,
-                h => sensor.IsAvailableChanged -= h
-                ).Select(e => e.EventArgs);
-        }
-        /// <summary>
-        /// Create AudioSouce stream from KinectSensor
-        /// </summary>
-        /// <param name="sensor">source of stream</param>
-        /// <returns>Observable AudioSource stream</returns>
-        public static IObservable<AudioBeamFrameArrivedEventArgs> AudioBeamFrameAsObservable(this KinectSensor sensor)
-        {
-            var reader = sensor.AudioSource.OpenReader();
-            disposables.Add(reader);
-            return reader.AsObservable();
-        }
-        /// <summary>
-        /// Create Body stream from KinectSensor
-        /// </summary>
-        /// <param name="sensor">souce of stream</param>
-        /// <returns>Observable Body frame stream</returns>
-        public static IObservable<BodyFrameArrivedEventArgs> BodyFrameAsObservable(this KinectSensor sensor)
-        {
-            var reader = sensor.BodyFrameSource.OpenReader();
-            disposables.Add(reader);
-            return reader.AsObservable();
-        }
-        /// <summary>
-        /// Create BodyIndex stream from KinectSensor
-        /// </summary>
-        /// <param name="sensor">souce of stream</param>
-        /// <returns>Observable BodyIndex frame stream</returns>
-        public static IObservable<BodyIndexFrameArrivedEventArgs> BodyIndexFrameAsObservable(this KinectSensor sensor)
-        {
-            var reader = sensor.BodyIndexFrameSource.OpenReader();
-            disposables.Add(reader);
-            return reader.AsObservable();
-        }
-        /// <summary>
-        /// Create ColorFrame stream from KinectSensor
-        /// </summary>
-        /// <param name="sensor">souce of stream</param>
-        /// <returns>Observable Color frame stream</returns>
-        public static IObservable<ColorFrameArrivedEventArgs> ColorFrameAsObservable(this KinectSensor sensor)
-        {
-            var reader = sensor.ColorFrameSource.OpenReader();
-            disposables.Add(reader);
-            return reader.AsObservable();
-        }
-        /// <summary>
-        /// Create Depth stream from KinectSensor
-        /// </summary>
-        /// <param name="sensor">souce of stream</param>
-        /// <returns>Observable Depth frame stream</returns>
-        public static IObservable<DepthFrameArrivedEventArgs> DepthFrameAsObservable(this KinectSensor sensor)
-        {
-            var reader = sensor.DepthFrameSource.OpenReader();
-            disposables.Add(reader);
-            return reader.AsObservable();
-        }
-        /// <summary>
-        /// Create Infrared stream from KinectSensor
-        /// </summary>
-        /// <param name="sensor">souce of stream</param>
-        /// <returns>Observable Infrared frame stream</returns>
-        public static IObservable<InfraredFrameArrivedEventArgs> InfraredFrameAsObservable(this KinectSensor sensor)
-        {
-            var reader = sensor.InfraredFrameSource.OpenReader();
-            disposables.Add(reader);
-            return reader.AsObservable();
-        }
-        /// <summary>
-        /// Create LongExposureInfrared stream from KinectSensor
-        /// </summary>
-        /// <param name="sensor">souce of stream</param>
-        /// <returns>Observable Long exposure infrared frame stream</returns>
-        public static IObservable<LongExposureInfraredFrameArrivedEventArgs> LongExposureInfraredFrameAsObservable(this KinectSensor sensor)
-        {
-            var reader = sensor.LongExposureInfraredFrameSource.OpenReader();
-            disposables.Add(reader);
-            return reader.AsObservable();
-        }
-        /// <summary>
-        /// Create MultiSource stream from KinectSensor
-        /// </summary>
-        /// <param name="sensor">souce of stream</param>
-        /// <param name="frameSouceTypes">frame types to observe</param>
-        /// <returns>Observable Multi source frame stream</returns>
-        public static IObservable<MultiSourceFrameArrivedEventArgs> MultiSourceFrameAsObservable(this KinectSensor sensor, FrameSourceTypes frameSouceTypes)
-        {
-            var reader = sensor.OpenMultiSourceFrameReader(frameSouceTypes);
-            disposables.Add(reader);
-            return reader.AsObservable();
-        }
-        /// <summary>
         /// Create VisualGestureBuilder stream from KinectSensor
         /// </summary>
         /// <param name="sensor">source of stream</param>
@@ -141,28 +28,6 @@ namespace K4wRx.Extensions
             reader.IsPaused = false;
             disposables.Add(reader);
             return reader.AsObservable();
-        }
-        /// <summary>
-        /// Create body object stream from KinectSensor
-        /// </summary>
-        /// <param name="sensor">source of stream</param>
-        /// <returns>Observable Body objects stream</returns>
-        public static IObservable<IEnumerable<Body>> BodyAsObservable(this KinectSensor sensor)
-        {
-            var reader = sensor.BodyFrameSource.OpenReader();
-            disposables.Add(reader);
-            return reader.BodyAsObservable();
-        }
-        /// <summary>
-        /// Create tracked body object stream from KinectSensor
-        /// </summary>
-        /// <param name="sensor">source of stream</param>
-        /// <returns>Observable tracked Body objects stream</returns>
-        public static IObservable<IEnumerable<Body>> TrackedBodyAsObservable(this KinectSensor sensor)
-        {
-            var reader = sensor.BodyFrameSource.OpenReader();
-            disposables.Add(reader);
-            return reader.TrackedBodyAsObservable();
         }
         /// <summary>
         /// Create VisualGestureBuilder's DiscreteGestureResult stream for tracked bodies.
@@ -225,7 +90,6 @@ namespace K4wRx.Extensions
         /// <param name="sensor">source of stream</param>
         /// <param name="databasePath">path to database</param>
         /// <returns>Observable ContinuousGestureResult frame stream for tracked bodies.</returns>
-
         public static IObservable<IEnumerable<IDictionary<Gesture, ContinuousGestureResult>>> TrackedBodyContinuousGestureResultsAsObservable(this KinectSensor sensor, string databasePath)
         {
             var frameStreams = Enumerable.Range(1, sensor.BodyFrameSource.BodyCount).Select(_ =>
@@ -274,39 +138,6 @@ namespace K4wRx.Extensions
                     }
                 }).ToList();
             });
-        }
-        /// <summary>
-        /// Create notifier for CoordinateMapper. This stream notices that CoordinateMapper is activated. 
-        /// </summary>
-        /// <param name="sensor">source of stream</param>
-        /// <returns>Observable CoordinateMapper activation </returns>
-        public static IObservable<CoordinateMapper> CoordinateMapperAsObservable(this KinectSensor sensor)
-        {
-            var reader = sensor.DepthFrameSource.OpenReader();
-            // CoordinateMapper is enabled when first depth frame is arrived
-            return reader.AsObservable().Select(e =>
-            {
-                reader.Dispose();
-                return sensor.CoordinateMapper;
-            });
-        }
-        /// <summary>
-        /// Dispose all readers which are listened by KinectSensorExtension.
-        /// Once you call this method, you *CANNOT* open any reader with KinectSensorExtension
-        /// </summary>
-        /// <param name="sensor">source of readers</param>
-        public static void DisposeLiteningReaders(this KinectSensor sensor)
-        {
-            disposables.Dispose();
-        }
-        /// <summary>
-        /// Dispose and clear all readers which are listened by KinectSensorExtension.
-        /// You *CAN* open other readers with KinectSensorExtension after you call it.
-        /// </summary>
-        /// <param name="sensor">source of readers</param>
-        public static void ClearListeningReaders(this KinectSensor sensor)
-        {
-            disposables.Clear();
         }
     }
 }
